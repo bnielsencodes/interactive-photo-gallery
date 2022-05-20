82f38ef6485a30e506225a2936acdbb32639cc0d
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-05-19 18:11:06 -0700

    final draft
    remove sass setup folders
    
    add layout spacing
    
    add lightbox images
    
    add css styling
    
    add form and searchbar attributes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-baguetteBox.run(".gallery");
+let inputLog = document.getElementById("search");
+
+inputLog.addEventListener("keyup", searchImage);
+
+function searchImage() {
+  filterValue = document.getElementById("search").value.toLowerCase();
+
+  const anchors = document.querySelectorAll("a");
+
+  for (i = 0; i < anchors.length; i++) {
+    let captions = anchors[i].getAttribute("data-caption").toLowerCase();
+    let filter = captions.includes(filterValue);
+
+    if (filter === true) {
+      anchors[i].style.display = "";
+    } else {
+      anchors[i].style.display = "none";
+    }
+  }
+}
